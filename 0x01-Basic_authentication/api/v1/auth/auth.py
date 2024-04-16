48d4f1f6a3ad7c665d461418fe7eadc4a2b537cc
felixsindani <felixsindani72@gmail.com>
2024-04-16 22:52:28 +0300

    tasks

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Auth class
+"""
+
+from typing import TypeVar, List
+from tabnanny import check
+from flask import request
+User = TypeVar('User')
+
+
+class Auth:
+    """
+    class managing API authentication
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Define which routes don't need authentication
+        """
+        check = path
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            check += "/"
+        if check in excluded_paths or path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        validate all requests to secure the API
+        """
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> User:
+        """
+        returns None - request
+        """
+        return None
